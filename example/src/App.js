d14977823a4b867d869a7cb2f421751fd787831a
Maze Peterson <mazecodes@protonmail.com>
2021-02-13 02:10:15 -0800

    init create-react-library@3.1.1

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'react-hash'
+import 'react-hash/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
